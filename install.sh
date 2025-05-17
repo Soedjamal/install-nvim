ddfb43cd9d455bc58679eb81fbd2bd3b637c0d0d
Akmal Ihksan <akmalihksan181@gmail.com>
2025-05-17 21:07:46 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Direktori konfigurasi Neovim
+CONFIG_DIR="$HOME/.config/nvim"
+
+# URL download template (Google Drive atau GitHub)
+DOWNLOAD_URL="https://drive.google.com/uc?export=download&id=1_cHxNOdpHwM7uHpiwl_gXSmCqy6AKfQI"
+
+# Nama file ZIP
+FILE_NAME="lazy-nvim-main.zip"
+
+# Unduh template
+echo "Mengunduh template..."
+wget --quiet --no-check-certificate "$DOWNLOAD_URL" -O "$FILE_NAME"
+
+# Buat direktori konfigurasi jika belum ada
+mkdir -p "$CONFIG_DIR"
+
+# Ekstrak file ke direktori konfigurasi Neovim
+unzip -o "$FILE_NAME" -d "$CONFIG_DIR"
+
+# Hapus file ZIP setelah ekstraksi
+rm "$FILE_NAME"
+
+# Sinkronisasi plugin dengan Lazy.nvim
+
+echo "Instalasi selesai. Silakan buka Neovim untuk mulai menggunakan template Anda."
