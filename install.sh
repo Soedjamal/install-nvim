22d21b8b290bb673628add46821cb8bd6454d987
Akmal Ihksan <akmalihksan181@gmail.com>
2025-06-21 12:26:38 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,15 @@
-#!/bin/bash
-
-CONFIG_DIR="$HOME/.config/nvim"
-DOWNLOAD_URL="https://drive.google.com/uc?export=download&id=1_Eph4mrpR5Y1rokDlEvfAslBr1LK42Y2"
-FILE_NAME="lazy-nvim-main.zip"
-TEMP_DIR="$HOME/.config/nvim-temp"
-
-echo "Mengunduh template..."
-wget --quiet --no-check-certificate "$DOWNLOAD_URL" -O "$FILE_NAME"
-
-# Buat folder sementara untuk ekstraksi isi zip
-mkdir -p "$TEMP_DIR"
-unzip -o "$FILE_NAME" -d "$TEMP_DIR"
-
-# Pindahkan isi folder hasil ekstrak ke direktori Neovim config
-mkdir -p "$CONFIG_DIR"
-cp -r "$TEMP_DIR/lazy-nvim-main/"* "$CONFIG_DIR/"
-
-# Bersihkan file sementara
-rm -rf "$TEMP_DIR"
-rm -f "$FILE_NAME"
-
+CONFIG_DIR="$HOME/.config/nvim" DOWNLOAD_URL="[https://drive.google.com/uc?export=download&id=1_Eph4mrpR5Y1rokDlEvfAslBr1LK42Y2]" FILE_NAME="lazy-nvim-main.zip"
+ 
+echo "Mengunduh template..." wget --quiet --no-check-certificate "$DOWNLOAD_URL" -O "$FILE_NAME"
+ 
+mkdir -p "$CONFIG_DIR" unzip -o "$FILE_NAME" -d "$CONFIG_DIR"
+ 
+# Salin semua isi dari lazy-nvim-main ke config dir
+ 
+cp -r "$CONFIG_DIR/lazy-nvim-main/"* "$CONFIG_DIR/"
+ 
+# Bersihkan sisa folder
+ 
+rm -rf "$CONFIG_DIR/lazy-nvim-main" rm "$FILE_NAME"
+ 
 echo "Instalasi selesai. Silakan buka Neovim untuk mulai menggunakan template Anda."
